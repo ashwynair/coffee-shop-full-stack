7d9d8fa020209a738cf33a9decbd65408c82bea6
ashwynnair <ashwynnair@deloitte.co.uk>
2020-04-14 10:29:56 +0100

    Endpoint for GET /drinks-detail requests

--- backend/src/api.py
+++ backend/src/api.py
@@ -36,14 +36,20 @@ def get_drinks():
     })
 
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail():
+    """
+    Requires get:drinks-detail permissions. Contains the drink.long() data representation
+    :return: status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    or appropriate status code indicating reason for failure
+    """
+    drinks = list(map(Drink.long, Drink.query.all()))
+    return jsonify({
+        "success": True,
+        "drinks": drinks
+    })
+
 
 '''
 @TODO implement endpoint
